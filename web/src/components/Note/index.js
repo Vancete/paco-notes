5212be7e1bb34166dcffa80d7f6344de1e187c51
adriank <adrian.cm.92@gmail.com>
2023-02-10 13:54:57 +0100

    changes in notes container and add note component and styles

---
+++ web/src/components/Note/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.scss";
+
+const Note = () => {
+  return <div className="note">Note</div>;
+};
+
+export default Note;
--- web/src/containers/Notes/index.js
+++ web/src/containers/Notes/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import "./styles.scss";
 
 import Header from "../../components/Header";
+import Note from "../../components/Note";
 
 const Notes = () => {
   return (
     <div className="notes">
       <Header />
+      <div className="notes-body">
+        <div className="notes-content"></div>
+        <Note />
+      </div>
     </div>
   );
 };
